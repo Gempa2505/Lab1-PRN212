e63f78e71aba85b7c59aa0523bb5041f772416a2
truonghaohiep <hiept2505@gmail.com>
2025-06-06 01:22:18 +0700

    Lab1-0606

---
+++ BusinessObjects/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessObjects
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public Category(int catID, string catName)
+        {
+            this.CateforyId = catID;
+            this.CategoryName = catName;
+        }
+
+        public int CateforyId { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
